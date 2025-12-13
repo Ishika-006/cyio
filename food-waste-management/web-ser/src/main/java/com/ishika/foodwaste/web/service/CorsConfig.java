4ec674c763d5904a9ff44bbbc713060bb5824666
Ishika-006 <ishikasharma9784@gmail.com>
2025-12-14 01:27:25 +0530

    Update CorsConfig.java

--- food-waste-management/web-ser/src/main/java/com/ishika/foodwaste/web/service/CorsConfig.java
+++ food-waste-management/web-ser/src/main/java/com/ishika/foodwaste/web/service/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("https://foodwaste2-frontend.netlify.app")
+                    .allowedOrigins("https://foodwaste3-frontend.netlify.app")
                     .allowedMethods("*")
                     .allowedHeaders("*")
                     .allowCredentials(true); // ✅ This is important
