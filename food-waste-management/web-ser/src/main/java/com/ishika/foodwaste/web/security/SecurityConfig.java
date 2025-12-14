453acf8f248972c84752e42daf40b481821b7358
Ishika-006 <ishikasharma9784@gmail.com>
2025-12-14 21:38:54 +0530

    Update SecurityConfig.java

--- food-waste-management/web-ser/src/main/java/com/ishika/foodwaste/web/security/SecurityConfig.java
+++ food-waste-management/web-ser/src/main/java/com/ishika/foodwaste/web/security/SecurityConfig.java
@@ -18,6 +18,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+              .cors()
+               .and()
             .csrf(csrf -> csrf.disable())
             
             .authorizeHttpRequests(auth -> auth
